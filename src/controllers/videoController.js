359624b69ac2ba3b93e900e635a90ca9940d77a9
Java-kokyu <qwe38280@gmail.com>
2021-07-27 18:04:38 +0900

    add html with pug

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,14 +1,13 @@
-export const trending = (req, res) => res.send("Home Page Video");
+const fakeUser = {
+    username: "Minji",
+    loggedIn: true,
+}
+
+export const trending = (req, res) => res.render("home", { pageTitle: "Home", fakeUser: fakeUser});
 export const search = (req, res) => res.send("search");
 
-export const watch = (req, res) => {
-    console.log(req.params);
-    return res.send(`Watch Video #${req.params.id}`);
-};
-export const edit = (req, res) => {
-    console.log(req.params);
-    return res.send("Edit");
-};
+export const watch = (req, res) => res.render("watch", { pageTitle: "Watch" });
+export const edit = (req, res) => res.render("edit", { pageTitle: "Edit" })
 export const remove = (req, res) => {
     console.log(req.params);
     return res.send("Delete Video")
--- src/routers/userRouter.js
+++ src/routers/userRouter.js
@@ -1,10 +1,12 @@
 import express from "express"
-import { edit, remove } from "../controllers/userController"
+import { edit, remove, logout, seeUser } from "../controllers/userController"
 
 
 const userRouter = express.Router();
 
 userRouter.get("/edit", edit);
-userRouter.get("/remove", remove)
+userRouter.get("/remove", remove);
+userRouter.get("/logout", logout)
+userRouter.get("/:id",seeUser);
 
 export default userRouter;
